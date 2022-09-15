b44c2061a0a633230c3794bb97cf3232c78c2535
Michael Baptiswa Marully <baptiswamichael@gmail.com>
2022-09-15 12:08:55 +0700

    Add some specifications for 2nd lab

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
+from django.core import serializers
+from django.http import HttpResponse
+
 # Create your views here.
 def show_wishlist(request):
+    data_barang_wishlist = BarangWishlist.objects.all()
+    context = {
+        'list_barang': data_barang_wishlist,
+        'nama': 'Michael Baptiswa'
+    }
     return render(request, "wishlist.html", context)
-data_barang_wishlist = BarangWishlist.objects.all()
-context = {
-    'list_barang': data_barang_wishlist,
-    'nama': 'Michael Baptiswa'
-}
+
+
+def show_xml(request):
+    data = BarangWishlist.objects.all()
+    return HttpResponse(serializers.serialize('xml', data), content_type="application/xml")
+
+def show_json(request):
+    data = BarangWishlist.objects.all()        
+    return HttpResponse(serializers.serialize('json', data), content_type="application/json")
+
+def show_json_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize('json', data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize('xml', data), content_type="application/xml")
